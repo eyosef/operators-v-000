0bd0ed62ae6f68bb055904e98f168a3725380889
Eden Mesfun <emesfun@gmail.com>
2018-01-23 17:17:32 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -9,6 +9,9 @@ def unsafe?(speed)
 end #method
 
 def not_safe?(speed)
-	speed > 60 ? true : false;
-	speed < 40 ? true : false;
+	if speed < 40 || speed > 60
+		return true 
+	elsif speed > 40 || speed < 60
+		false;
+	end 
 end
