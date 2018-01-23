30b6647934bcf7b0c9388c35353017bf98af4613
Eden Mesfun <emesfun@gmail.com>
2018-01-23 17:14:15 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,8 +1,8 @@
 require "pry"
 
 def unsafe?(speed)
-	speed <= 40 ? true : false;
-	speed >= 60 ? true : false;
+	speed < 40 ? true : false;
+	speed > 60 ? true : false;
 end #method
 
 def not_safe?(speed)
